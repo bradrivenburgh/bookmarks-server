62644898eda543aeed16955d7d791efa76127e05
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-09-07 11:27:36 -0400

    created /bookmarks/:id endpoint

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -15,6 +15,26 @@ bookmarksRouter
   })
 
 // Set up /bookmarks/:id router / endpoint
+bookmarksRouter
+  .route('/bookmarks/:id')
+  .get((req, res) => {
+    // Get the id param
+    const { id } = req.params;
+    // Find the first bookmark with correct id from the bookmarks list
+    const bookmark = bookmarks.find(b => b.id === id);
+
+    // Validate the bookmark existence.  If not, return and log 404 error / message
+    if (!bookmark) {
+      logger.error(`Bookmark with id ${id} does not exist`)
+      return res
+        .status(404)
+        .json({ error: 'Bookmark not found'});
+    }
+
+    // Return the bookmark in json format
+    res.json(bookmark);
+  })
+
 
 module.exports = {
   bookmarksRouter: bookmarksRouter
