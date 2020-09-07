bb21d82f7f2cc5e2e981b8e706c064b4fc3d5e57
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-09-07 12:01:57 -0400

    created /bookmarks POST endpoint

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -13,6 +13,49 @@ bookmarksRouter
   .get((req, res) => {
     res.json(bookmarks);
   })
+  .post(bodyParser, (req, res) => {
+    // Get the data from the request body; default values for optional props
+    const { title, url, description='', rating=0 } = req.body;
+    console.log(title)
+    // Validate the data: title and url required
+    if (!title) {
+      logger.error('Title is required');
+      return res
+        .status(400)
+        .json({ error: 'Invalid data' });
+    }
+
+    if (!url) {
+      logger.error('URL is required');
+      return res
+        .status(400)
+        .json({ error: 'Invalid data' });
+    }
+
+    // Generate a unique id
+    const id = uuid();
+
+    // Create the bookmark data
+    const bookmark = {
+      id,
+      title,
+      url,
+      description,
+      rating
+    }
+
+    // Add the bookmark to the bookmarks list
+    bookmarks.push(bookmark);
+
+    // Log bookmark creation 
+    logger.info(`Card with the id ${id} created`);
+    
+    // Send response with location header and created bookmark
+    res
+      .status(201)
+      .location(`http://localhost:8000/bookmarks/${id}`)
+      .json(bookmark);
+  });
 
 // Set up /bookmarks/:id router / endpoint
 bookmarksRouter
@@ -31,7 +74,7 @@ bookmarksRouter
         .json({ error: 'Bookmark not found'});
     }
 
-    // Return the bookmark in json format
+    // Send requested bookmark in json format
     res.json(bookmark);
   })
 
