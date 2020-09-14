1f659a471a0e4bc73899f94ccecffea3a075d82d
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-09-14 15:48:29 -0400

    seeded bookmarks table in bookmarks db

--- seeds/seed.boomkarks.sql
+++ seeds/seed.boomkarks.sql
@@ -2,15 +2,15 @@ BEGIN;
 
 INSERT INTO bookmarks (title, url, description, rating)
 VALUES
-  ('Google', 'https://www.google.com', 'Dolor est minim est culpa nisi laboris aliquip quis in ipsum nulla.', 5)
-  ('Mr. Money Mustache', 'https://www.mrmoneymustache.com/', 'Dolor est minim est culpa nisi laboris aliquip quis in ipsum nulla.', 5)
-  ('Mad Fientist', 'https://www.madfientist.com/', 'Dolor est minim est culpa nisi laboris aliquip quis in ipsum nulla.', 5)
-  ('Portfolio Charts', 'https://portfoliocharts.com/', 'Dolor est minim est culpa nisi laboris aliquip quis in ipsum nulla.', 5)
-  ('JL Collins', 'https://jlcollinsnh.com/', 'Dolor est minim est culpa nisi laboris aliquip quis in ipsum nulla.', 5)
-  ('Wait But Why', 'https://waitbutwhy.com/', 'Dolor est minim est culpa nisi laboris aliquip quis in ipsum nulla.', 5)
-  ('Coach Carson', 'https://www.coachcarson.com/', 'Dolor est minim est culpa nisi laboris aliquip quis in ipsum nulla.', 5)
-  ('Codecademy', 'https://www.codecademy.com/', 'Dolor est minim est culpa nisi laboris aliquip quis in ipsum nulla.', 5)
-  ('FreeCodeCamp', 'https://www.freecodecamp.org/', 'Dolor est minim est culpa nisi laboris aliquip quis in ipsum nulla.', 5)
-  ('Udemy', 'https://www.udemy.com/', 'Dolor est minim est culpa nisi laboris aliquip quis in ipsum nulla.', 5)
+  ('Google', 'https://www.google.com', 'Dolor est minim est culpa nisi laboris aliquip quis in ipsum nulla.', 5),
+  ('Mr. Money Mustache', 'https://www.mrmoneymustache.com/', 'Dolor est minim est culpa nisi laboris aliquip quis in ipsum nulla.', 5),
+  ('Mad Fientist', 'https://www.madfientist.com/', 'Dolor est minim est culpa nisi laboris aliquip quis in ipsum nulla.', 5),
+  ('Portfolio Charts', 'https://portfoliocharts.com/', 'Dolor est minim est culpa nisi laboris aliquip quis in ipsum nulla.', 5),
+  ('JL Collins', 'https://jlcollinsnh.com/', 'Dolor est minim est culpa nisi laboris aliquip quis in ipsum nulla.', 5),
+  ('Wait But Why', 'https://waitbutwhy.com/', 'Dolor est minim est culpa nisi laboris aliquip quis in ipsum nulla.', 5),
+  ('Coach Carson', 'https://www.coachcarson.com/', 'Dolor est minim est culpa nisi laboris aliquip quis in ipsum nulla.', 5),
+  ('Codecademy', 'https://www.codecademy.com/', 'Dolor est minim est culpa nisi laboris aliquip quis in ipsum nulla.', 5),
+  ('FreeCodeCamp', 'https://www.freecodecamp.org/', 'Dolor est minim est culpa nisi laboris aliquip quis in ipsum nulla.', 5),
+  ('Udemy', 'https://www.udemy.com/', 'Dolor est minim est culpa nisi laboris aliquip quis in ipsum nulla.', 5);
 
 COMMIT;
